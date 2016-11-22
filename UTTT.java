7aa5b662a256eea7ab9e9a9e667802e7b6289dbe
ARineer <apr810@gmail.com>
2016-11-22 09:35:52 -0700

    Update UTTT.java
    Mouse clicks now register exact box location (returns string)

--- UTTT.java
+++ UTTT.java
@@ -13,13 +13,13 @@ public class UTTT{
     public static final int WIDTH = 800;
     public static final int HEIGHT = 800;
     public static final String TITLE = "Ultamite Tic Tac Toe!";
+    public static Box newBoxes[][][][] = new Box[3][3][3][3]; 
     public static Box boxes[][][][] = new Box[3][3][3][3];
     public static Box big_boxes[][] = new Box[3][3];
     //Variables used for drawing
     public static List<Line2D> drawLines = new ArrayList<Line2D>();
     public static Box hoverBox;
     public static List<Box> availableToPlay = new ArrayList<Box>();
-
 
     public static void main(String[] args){
         //-----Initialize Lines-----
@@ -47,10 +47,10 @@ public class UTTT{
             for (int j=0;j<3;j++){
                 for (int k=0;k<3;k++){
                     for (int l=0;l<3;l++){
-                        boxes[i][j][k][l] = uttt.new Box((x_start+27+(55*l + j*x_offset)), (y_start+27+(55*k + i*y_offset)), 50, 50, i+1, j+1);
+                        boxes[i][j][k][l] = uttt.new Box((x_start+27+(55*l + j*x_offset)), (y_start+27+(55*k + i*y_offset)), 50, 50, i+1, j+1, k+1, l+1);
                     }
                 }
-                big_boxes[i][j] = uttt.new Box(x_start+(j*x_offset),y_start+(i*y_offset),215,215, i+1, j+1);
+                big_boxes[i][j] = uttt.new Box(x_start+(j*x_offset),y_start+(i*y_offset),215,215, i+1, j+1, 0, 0);
             }
         }
 
@@ -59,7 +59,7 @@ public class UTTT{
         // Now the fun part
         JFrame jf = new JFrame(TITLE);
         Container cp = jf.getContentPane();
-
+        
         cp.add(new JComponent() { //Our lovely component that does all the work
             public void paintComponent(Graphics g) {
                 /*Basically, we have 1 paint statement, and it draws things that are
@@ -96,11 +96,30 @@ public class UTTT{
                 }
             }
         });
+        
         // Now we add MouseListeners to that component
         cp.addMouseListener(new MouseListener() {
             @Override
             public void mouseClicked(MouseEvent e) {
-                selectBox(e.getX(), e.getY(), new Color(0,0,255), cp);
+            	System.out.println(sendBoxPosition(e.getX(), e.getY()));
+            	
+            	/*int x_start = 45;
+                int y_start = 45;
+                int y_offset = 230;
+                int x_offset = 230;
+                  
+                //create variables for big box and small box sizes
+                int x = (e.getX() - x_start) / x_offset; 
+                int y = (e.getY() - y_start) / y_offset;
+                int mx = e.getX() - x * x_offset - x_start;
+                int my = e.getY() - y * y_offset - y_start;
+                int little_x = (mx - 27) / 55;
+                int little_y = (my - 27) / 55;
+                //selectBox(e.getX(), e.getY(), new Color(0,0,255), cp); //changes box color to blue
+                
+                //display exact location of mouse click within big box and little box
+                System.out.println("Mouse click location: " + (y+1) + ","+ (x+1) + "," + (little_y+1) + "," + (little_x+1)); 
+                */
             }
             //Unused methods, but still need to override them
             @Override
@@ -108,9 +127,12 @@ public class UTTT{
             @Override
             public void mouseExited(MouseEvent e) { }
             @Override
-            public void mousePressed(MouseEvent e) { }
-            @Override
             public void mouseReleased(MouseEvent e) { }
+			@Override
+			public void mousePressed(MouseEvent e) {
+				// TODO Auto-generated method stub
+				
+			}
         });
         cp.addMouseMotionListener(new MouseMotionListener() {
             @Override
@@ -124,17 +146,19 @@ public class UTTT{
         jf.setSize(WIDTH, HEIGHT);
         jf.setVisible(true);
     }
-
+    
     class Box {
-        private int x, y, height, width, big_row, big_col;
+        private int x, y, height, width, big_row, big_col, little_row, little_col;
         private Color color;
-        Box(int x, int y, int width, int height, int big_row, int big_col) {
+        Box(int x, int y, int width, int height, int big_row, int big_col, int little_row, int little_col) {
             this.x = x;
             this.y = y;
             this.height = height;
             this.width = width;
             this.big_row = big_row;
             this.big_col = big_col;
+            this.little_row = little_row;
+            this.little_col = little_col;
             //System.out.println("("+x+", "+y+", "+(x+width)+", "+(y+height)+")");
 
         }
@@ -157,6 +181,12 @@ public class UTTT{
         public int getBigCol(){
             return big_col;
         }
+        public int getLittleRow(){
+            return little_row;
+        }
+        public int getLittleCol(){
+            return little_col;
+        }
         public Color getColor(){
             return color;
         }
@@ -167,7 +197,7 @@ public class UTTT{
         }
     }
 
-    public static Box findBox(int x, int y) { //We use this process quite a bit, so it makes sense to make it a function
+    public static Box findBox(int x, int y)  { //We use this process quite a bit, so it makes sense to make it a function
         Boolean found = false;
         for (int i = 0; i<3; i++){
             for (int j = 0; j<3; j++){
@@ -179,9 +209,9 @@ public class UTTT{
                         double right = left + boxes[i][j][k][l].getWidth();
                         //System.out.println(i+":"+j+" (" + (int) left + ", "+(int) top+", "+(int) right+", "+(int) bottom+")");
                         if (y > top && y < bottom && x > left && x < right) {
+                        	//System.out.println("Correct Location: " + (i+1) + ","+(j+1) + ","+(k+1)+","+(l+1));
                             found = true; 
-                            return boxes[i][j][k][l];
-                            //System.out.println("Yep!" + (i+1) + ","+(j+1) + ":"+(k+1)+","+(l+1));
+                        	return boxes[i][j][k][l];
                         }
                     }
                 }
@@ -193,7 +223,6 @@ public class UTTT{
     /* Find what box we're pointing at. If it's not a box, it returns null
     ** If we're still pointing at the same box, there's no need to repaint the screen,
     ** so only repaint if the old_box is different from the new box.
-
     */
     public static void highlightBox(int x, int y, Color new_color, Container cp) {
         Box old_box = hoverBox;
@@ -201,13 +230,15 @@ public class UTTT{
         if (old_box != hoverBox) {
             if (hoverBox != null) {
                 hoverBox.changeColor(new_color);
-                if (hoverBox.getBigRow() != 2 || hoverBox.getBigCol() != 2) { //Demoing out how we can restrict them to play in the correct spot on the board
-                    hoverBox = null;
-                }
+                //System.out.println(hoverBox.getBigRow()+", " + hoverBox.getBigCol()+ "| " + hoverBox.getLittleRow()+ ", "+ hoverBox.getLittleCol());
+                //if (hoverBox.getBigRow() != 2 || hoverBox.getBigCol() != 2) { //Demoing out how we can restrict them to play in the correct spot on the board
+                  //  hoverBox = null;
+                //}
             }
             cp.repaint();
         }
     }
+    
     public static void selectBox(int x, int y, Color new_color, Container cp) {
         Box temp = findBox(x,y);
         if (temp != null) {
@@ -215,5 +246,14 @@ public class UTTT{
             availableToPlay.add(big_boxes[temp.getBigRow()-1][temp.getBigCol()-1]);
         }
 
+    }
+    
+    public static String sendBoxPosition(int x, int y) {
+    	Box temp = findBox(x,y);
+    	if ( temp != null) {
+        	return (temp.getBigRow()+", " + temp.getBigCol()+ "| " + temp.getLittleRow()+ ", "+ temp.getLittleCol());
+    	} else {
+    		return "";
+    	}
     }
 }
