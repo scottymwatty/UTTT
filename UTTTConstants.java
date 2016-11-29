35bafd0ea50724cb1f020396a0b89c9aec6c2b21
JARVIS\Jason <upserge@gmail.com>
2016-11-28 21:23:06 -0700

    Add Constants file (Might trim down constants later to optomize)

---
+++ UTTTConstants.java
@@ -0,0 +1,20 @@
+/*
+* Created by: Jason Salas
+* Last edited by: Jason Salas
+* Written on: 11/7/16
+* Assignment: Ultimate Tic Tac Toe - Constants Folder
+*/
+public interface UTTTConstants 
+{
+     //Global Constant Variables
+    public static int Player1 = 1;  //Player 1 represented as 1
+    public static int Player2 = 2;  //Player 2 represented as 2
+    public static int Player1_Won = 1;  //Player 1 winning is 1
+    public static int Player2_Won = 2;  //Player 2 winning is 2
+    public static int Draw = 3;     //Draw is represented as 3
+    public static int Continue = 4; //Continue state is represented as 4
+    public static char[] xCharArray = new char['X'];
+    public static char[] oCharArray = new char['O'];
+    public static char xChar = 'X';
+    
+}
